e08ffedf4339b9b7ee4d9eb07d7040fa5925007c
Ksenchik <oksana.100@mail.ru>
2022-08-25 13:46:32 +0300

    fixed: berger menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // burger
-$(document).ready(function () {
-   $('.header__burger').click(function(event){
-      $('.header__burger, .header__menu').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
+let header__burger = document.querySelectorAll('.header__burger,.header__link');
+let header_menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
 
-
+header__burger.forEach(function (item) {
+   item.onclick = function () {
+      item.classList.toggle('active');
+      header_menu.classList.toggle('active');
+      back.classList.toggle('lock');
+   }
 });
